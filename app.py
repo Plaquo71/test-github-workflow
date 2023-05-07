eeb2f7f1a5149b5ae2a46d0f240b21869df3fe56
vdi-terminal <plaquo71@gmail.com>
2023-05-07 15:37:47 +0200

    Should work

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
+"""
+WSB Flask app.
+"""
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    # wiadomość Hello #
+    """
+    Returns a greeting message.
+    """
     return '<h1>Hello WSB! Greetings from Flask!</h1>'
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-

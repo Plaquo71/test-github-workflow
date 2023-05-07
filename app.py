dd8bf914a7dffcdcfc1e07dcff55413b9f781c61
vdi-terminal <plaquo71@gmail.com>
2023-05-07 15:23:48 +0200

    .

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
+    # wiadomość #
     return '<h1>Hello WSB! Greetings from Flask!</h1>'
 
 if __name__ == "__main__":
